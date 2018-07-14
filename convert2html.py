60f9ce8920a110c9f51021a667156d268d750d78
Drew Griffiths <drew@cotechteam.com>
2018-07-14 03:13:00 -0600

    added latest file for auto populate

--- convert2html.py
+++ convert2html.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python
 
-import 
+import pdf2txt as pdfcon
+import glob
+import os
+
+list_of_files = glob.glob('/home/media/drew/Projects/library/readable/*.pdf') # * means all if need specific format then *.csv
+latest_file = max(list_of_files, key=os.path.getctime)
+print (latest_file)
