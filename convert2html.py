239a161e5d726069a2091c5557759520d32dd390
Drew Griffiths <drew@cotechteam.com>
2018-07-18 02:24:19 -0600

    path change

--- convert2html.py
+++ convert2html.py
@@ -4,6 +4,6 @@ from pdfminertool.tools import pdf2txt as pdfcon
 import glob
 import os
 
-list_of_files = glob.glob('/home/media/drew/Projects/library/readable/*.pdf') # * means all if need specific format then *.csv
+list_of_files = glob.glob('/media/drew/Projects/library/readable/*.pdf') # * means all if need specific format then *.csv
 latest_file = max(list_of_files, key=os.path.getctime)
 print (latest_file)
