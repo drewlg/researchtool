5a86951a2e2bb51acde39fd187624acb742a4934
Drew Griffiths <drew@cotechteam.com>
2018-07-19 02:27:34 -0600

    ectract to html

--- convert2html.py
+++ convert2html.py
@@ -1,9 +1,37 @@
 #!/usr/bin/python
 import sys
 from pdfminertool.tools import pdf2txt as pdfcon
+from pdfminertool.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminertool.converter import TextConverter
+from pdfminertool.layout import LAParams
+from pdfminertool.pdfpage import PDFPage
+from io import StringIO
+
 import glob
 import os
 
 list_of_files = glob.glob('/media/drew/Projects/library/readable/*.pdf') # * means all if need specific format then *.csv
 latest_file = max(list_of_files, key=os.path.getctime)
-print (latest_file)
+
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = open(latest_file, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
